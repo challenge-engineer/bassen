db696eaa8fd0710e8a79afeb7ddbb6e6c3a5aa35
challenge-engineer <challenge.engineer.k.s@gmail.com>
2020-07-25 18:29:21 +0900

    Initial Commit

---
+++ omikuji/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  omikuji
+//
+//  Created by 酒井健太郎 on 2020/07/25.
+//  Copyright © 2020 酒井 健太郎. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
