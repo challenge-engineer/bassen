7357447f2bf0dcd6ad91f0fb3c42958f2e1359c0
challenge-engineer <challenge.engineer.k.s@gmail.com>
2020-07-26 13:09:54 +0900

    first commit

--- omikuji/ContentView.swift
+++ omikuji/ContentView.swift
@@ -9,13 +9,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    let Batting = ["絶好調","1安打","練習!!!","凡打","ホームラン"]
+    @State private var kuji = ""
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Button(action: {
+                self.kuji = self.Batting.randomElement()!
+            }){
+                Text("バッティング運勢")
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Capsule()
+                        .foregroundColor(.yellow)
+                )
+            }
+            Text(kuji)
+                .font(.system(size:50))
+                .fontWeight(.bold)
+                .foregroundColor(.red)
+        }
     }
 }
+
+//struct ContentView: View {
+//    @State private var showText = false
+//    var body: some View {
+//        VStack {
+//            Button(action: {
+//                self.showText.toggle()
+//            }) {
+//                HStack{
+//                    Image(systemName: "lightbulb")
+//                    Text("オン/オフ")
+//                }
+//            }
+//            if showText {
+//            Text("ステートテスト")
+//                .font(.largeTitle)
+//            }
+//        }
+//    }
+//}
+
+//struct ContentView: View {
+//    @State private var fast = true
+//    var body: some View {
+//        Button(action: {
+//            self.fast.toggle()
+//        }){
+//            Image(systemName: self.fast ? "hare" : "tortoise")
+//            .resizable()
+//            .scaledToFit()
+//                .frame(width:150, height: 150)
+//        }
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
